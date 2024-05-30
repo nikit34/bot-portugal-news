a28bbb906f73a2d00ff127f4afb50721a9615c54
nikit34 <permikov134@mail.ru>
2024-05-30 12:50:29 +0100

    Revert source

--- static/sources.py
+++ static/sources.py
@@ -1,16 +1,16 @@
 self_telegram_channel = 'https://t.me/sportportugal'
 
 telegram_channels = {
-    # 1006869491: 'https://t.me/sportsru',
+    1006869491: 'https://t.me/sportsru',
     # 2178707665: 'https://t.me/+M-LLGrMDui1hZjhi'  # For debug
 }
 
 rss_channels = {
-    # 'sport.ru': 'https://www.sport.ru/rssfeeds/news.rss',
-    # 'abola.pt': 'https://www.abola.pt/api/rss',
-    # 'abola.pt/nacional': 'https://www.abola.pt/rss/nacional',
-    # 'abola.pt/internacional': 'https://www.abola.pt/rss/internacional',
-    # 'abola.pt/modalidades': 'https://www.abola.pt/rss/modalidades',
-    # 'abola.pt/motores': 'https://www.abola.pt/rss/motores',
+    'sport.ru': 'https://www.sport.ru/rssfeeds/news.rss',
+    'abola.pt': 'https://www.abola.pt/api/rss',
+    'abola.pt/nacional': 'https://www.abola.pt/rss/nacional',
+    'abola.pt/internacional': 'https://www.abola.pt/rss/internacional',
+    'abola.pt/modalidades': 'https://www.abola.pt/rss/modalidades',
+    'abola.pt/motores': 'https://www.abola.pt/rss/motores',
     'bbc.com/football': 'https://feeds.bbci.co.uk/sport/football/rss.xml'
 }
