5e3861bef117561806bd5c1af1974f7b8826e15d
nikit34 <permikov134@mail.ru>
2024-05-31 00:17:59 +0100

    Add uuid

--- src/parsers/channels/formatter.py
+++ src/parsers/channels/formatter.py
@@ -1,11 +1,14 @@
 from io import BytesIO
 
+import uuid
 import requests
 from PIL import Image
 
 
 def change_format_image(url_image):
     response = requests.get(url_image)
-    im = Image.open(BytesIO(response.content))
-    im.save('tmp/image.jpg')
-    return 'tmp/image.jpg'
+    image = Image.open(BytesIO(response.content))
+    rgb_image = image.convert('RGB')
+    unique_id = str(uuid.uuid4())
+    rgb_image.save('tmp/image-' + unique_id + '.jpg')
+    return 'tmp/image' + unique_id + '.jpg'
