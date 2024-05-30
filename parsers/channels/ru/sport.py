6b4641cfcdd4e72821c781aef6ea2749601fb8d3
nikit34 <permikov134@mail.ru>
2024-05-30 03:10:38 +0100

    Add re

--- parsers/channels/ru/sport.py
+++ parsers/channels/ru/sport.py
@@ -1,3 +1,6 @@
+import re
+
+
 def check_sport_ru(entry):
     required_keys = ('summary', 'title', 'media_content', 'link')
     return not all(entry.get(key) for key in required_keys)
@@ -7,7 +10,8 @@ def parse_sport_ru(entry):
     summary = entry.get('summary')
     title = entry.get('title')
     message = title + '\n' + summary
+    cleaned_message = re.sub(r'</?div>|<br>', '', message)
 
     link = entry.get('link')
     image = entry.get('media_content')[0].get('url')
-    return message, link, image
+    return cleaned_message, link, image
