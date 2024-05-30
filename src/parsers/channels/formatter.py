7a92fc0713687ae15e75aeb9447b892eb143e1bd
nikit34 <permikov134@mail.ru>
2024-05-31 00:30:54 +0100

    Disable save to disk

--- src/parsers/channels/formatter.py
+++ src/parsers/channels/formatter.py
@@ -1,6 +1,5 @@
 from io import BytesIO
 
-import uuid
 import requests
 from PIL import Image
 
@@ -9,6 +8,9 @@ def change_format_image(url_image):
     response = requests.get(url_image)
     image = Image.open(BytesIO(response.content))
     rgb_image = image.convert('RGB')
-    unique_id = str(uuid.uuid4())
-    rgb_image.save('tmp/image-' + unique_id + '.jpg')
-    return 'tmp/image' + unique_id + '.jpg'
+
+    image_io = BytesIO()
+    rgb_image.save(image_io, format='JPEG')
+    image_io.seek(0)
+
+    return image_io
