3b32257a6f538ea6c06c828c2b8ac8fa9a24b644
nikit34 <permikov134@mail.ru>
2024-05-30 18:51:37 +0100

    Refactoring

--- src/parsers/self_telegram.py
+++ src/parsers/self_telegram.py
@@ -6,9 +6,12 @@ async def get_messages_history(getter_client):
     history = []
     async for message in getter_client.iter_messages(self_telegram_channel, limit=COUNT_UNIQUE_MESSAGES):
         raw_message = message.raw_text
-        if raw_message is None:
+        if not raw_message:
             continue
-        post = raw_message.split('\n', maxsplit=1)[1]
+        lines = raw_message.split('\n', maxsplit=1)
+        if len(lines) < 2:
+            continue
+        post = lines[1]
         cropped_post = post[:KEY_SEARCH_LENGTH_CHARS].strip()
         history.append(cropped_post)
     return history
