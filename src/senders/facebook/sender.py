6d167b4b91dcbfa00930dc38bc473c3f5a0c7279
nikit34 <permikov134@mail.ru>
2024-06-18 17:56:26 +0100

    Link dont work

--- src/senders/facebook/sender.py
+++ src/senders/facebook/sender.py
@@ -4,5 +4,5 @@ import facebook as fb
 
 
 graph = fb.GraphAPI('EAAYsLDJCjQoBO2ztSL63wmXwMny5WjlzoDig1FoQ2HJDQYKrsfpLQSdyqfZBZBQudwFns74yTJvdZB9WUoObOwoGwHFrftBBTYXlbeNzD2ZBRFdreXXbZBXXiVDnqS3YcOi0ynWcPu6Ibpjqvn1al8jxRGSoTz9txA4RZCTgOKrZBRd4VgAzI85YeTKA3iD1pAntL1T4nfRRc7JVKwGrRPZACpXwwsVUImnn0fIZChsUx')
-post = graph.put_photo(image=open('t.jpg', 'rb'), message='test11', link="https://nikit34.github.io")
+post = graph.put_photo(image=open('t.jpg', 'rb'), message='test11')
 graph.put_object(parent_object=post.get('id'), connection_name="comments", message="еуты")
