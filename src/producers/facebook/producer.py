2001a74a6daf45261d2abe93af4ce77df77ee0a2
nikit34 <permikov134@mail.ru>
2024-06-19 19:09:01 +0100

    Separate MAX_LENGTH_MESSAGE

--- src/producers/facebook/producer.py
+++ src/producers/facebook/producer.py
@@ -2,14 +2,14 @@ from time import sleep
 
 import pyshorteners
 
-from src.static.settings import TIMEOUT, MAX_LENGTH_MESSAGE, REPEAT_REQUESTS
+from src.static.settings import TIMEOUT, FACEBOOK_MAX_LENGTH_MESSAGE, REPEAT_REQUESTS
 from src.text_editor import trunc_str
 
 
 def facebook_prepare_post(translated_message, link):
     shortener = pyshorteners.Shortener()
     shorted_link = shortener.tinyurl.short(link)
-    return shorted_link + trunc_str(translated_message, MAX_LENGTH_MESSAGE)
+    return trunc_str(translated_message, FACEBOOK_MAX_LENGTH_MESSAGE) + '\n\n' + shorted_link
 
 
 async def facebook_send_message(graph, message, file, repeat=REPEAT_REQUESTS):
@@ -23,4 +23,4 @@ async def facebook_send_message(graph, message, file, repeat=REPEAT_REQUESTS):
 
 
 async def facebook_send_translated_respond(graph, flag, post, translated_text):
-    graph.put_object(parent_object=post.get('id'), connection_name="comments", message=flag + ' ' + trunc_str(translated_text, MAX_LENGTH_MESSAGE))
+    graph.put_object(parent_object=post.get('id'), connection_name="comments", message=flag + ' ' + trunc_str(translated_text, FACEBOOK_MAX_LENGTH_MESSAGE))
--- src/producers/telegram/producer.py
+++ src/producers/telegram/producer.py
@@ -1,12 +1,12 @@
 import asyncio
 
-from src.static.settings import MAX_LENGTH_MESSAGE, TIMEOUT, REPEAT_REQUESTS
+from src.static.settings import TELEGRAM_MAX_LENGTH_MESSAGE, TIMEOUT, REPEAT_REQUESTS
 from src.text_editor import trunc_str
 
 
 def telegram_prepare_post(translated_message, source, link):
     title_post = '<a href="' + link + '">' + source + '</a>\n'
-    return title_post + trunc_str(translated_message, MAX_LENGTH_MESSAGE)
+    return title_post + trunc_str(translated_message, TELEGRAM_MAX_LENGTH_MESSAGE)
 
 
 async def telegram_send_message(client, telegram_chat_id, post, file, repeat=REPEAT_REQUESTS):
@@ -26,4 +26,4 @@ async def telegram_send_message(client, telegram_chat_id, post, file, repeat=REP
 
 
 async def telegram_send_translated_respond(flag, message_sent, translated_text):
-    await message_sent.respond(flag + ' ' + trunc_str(translated_text, MAX_LENGTH_MESSAGE), comment_to=message_sent.id)
+    await message_sent.respond(flag + ' ' + trunc_str(translated_text, TELEGRAM_MAX_LENGTH_MESSAGE), comment_to=message_sent.id)
