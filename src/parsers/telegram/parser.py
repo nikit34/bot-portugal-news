6c73e59faa2660b94674b88beb7b79363d80df5c
nikit34 <permikov134@mail.ru>
2024-11-17 23:44:12 +0000

    Remove icon

--- src/parsers/telegram/parser.py
+++ src/parsers/telegram/parser.py
@@ -16,7 +16,7 @@ async def telegram_wrapper(getter_client, graph, nlp, translator, telegram_bot_t
     try:
         await _telegram_parser(getter_client, graph, nlp, translator, channel, posted_q)
     except Exception as e:
-        message = '&#9888; ERROR: ' + channel + ' telegram parser is down\n' + str(e) + ', response: ' + getattr(e, 'response', {}).get('content', '')
+        message = 'ERROR: ' + channel + ' telegram parser is down\n' + str(e) + ', response: ' + getattr(e, 'response', {}).get('content', '')
         logger.error(message)
         await send_message_api(message, telegram_bot_token)
 
