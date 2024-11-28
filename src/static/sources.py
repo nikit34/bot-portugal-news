471e485107beb8fd1adf4f08be2e0a31e2d26cbe
nikit34 <permikov134@mail.ru>
2024-11-28 03:00:26 +0000

    Disable

--- src/static/sources.py
+++ src/static/sources.py
@@ -5,7 +5,7 @@ self_facebook_page_id = '348454375016310'
 self_instagram_channel = '17841467413345329'
 
 telegram_channels = {
-    1193946403: "https://t.me/euro_football_ru",
+    # 1193946403: "https://t.me/euro_football_ru",
     # 2178707665: 'https://t.me/+M-LLGrMDui1hZjhi'  # For debug
 }
 
