966f8324d743c2ca2f30f09dbea416a7be8a450a
Nikita Permiakov <nikita.permiakov@fundraiseup.com>
2025-02-19 14:52:13 +0000

    Remove RSS and Instagram modules

--- src/static/sources.py
+++ src/static/sources.py
@@ -2,25 +2,14 @@ import os
 
 telegram_debug_chat_id = '-1002178707665'
 self_facebook_page_id = '348454375016310'
-self_instagram_channel = '17841467413345329'
 
 telegram_channels = {
     1193946403: "https://t.me/euro_football_ru",
     # 2178707665: 'https://t.me/+M-LLGrMDui1hZjhi'  # For debug
 }
 
-rss_channels = {
-    'abola.pt': 'https://www.abola.pt/api/rss',
-    'abola.pt/nacional': 'https://www.abola.pt/rss/nacional',
-    'abola.pt/internacional': 'https://www.abola.pt/rss/internacional',
-    'abola.pt/modalidades': 'https://www.abola.pt/rss/modalidades',
-    'abola.pt/motores': 'https://www.abola.pt/rss/motores',
-    'bbc.com/football': 'https://feeds.bbci.co.uk/sport/football/rss.xml'
-}
-
 tmp_folder = os.getcwd() + '/tmp'
 
 platforms = {
     'facebook': True,
-    'instagram': True,
 }
