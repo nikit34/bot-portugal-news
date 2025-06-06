0918c0c31f5f75a599c952897116d5233e3087f1
Никита Пермяков <permikov134@mail.ru>
2025-06-06 02:51:46 +0100

    refactor: remove commented-out telegram and rss channel links

--- src/static/sources.py
+++ src/static/sources.py
@@ -7,8 +7,6 @@ self_instagram_channel = '17841467413345329'
 telegram_channels = [
     # 'https://t.me/+M-LLGrMDui1hZjhi'  # For debug
 
-    # "https://t.me/euro_football_ru",
-    # 'https://t.me/MemesFutebol',
     'https://t.me/AO_VIVO_Futebol',
     'https://t.me/FutebolDaZoeira',
     'https://t.me/futebol_portugues',
@@ -19,8 +17,6 @@ rss_channels = {
     'abola.pt': 'https://www.abola.pt/api/rss',
     'abola.pt/nacional': 'https://www.abola.pt/rss/nacional',
     'abola.pt/internacional': 'https://www.abola.pt/rss/internacional',
-    # 'abola.pt/modalidades': 'https://www.abola.pt/rss/modalidades',
-    # 'abola.pt/motores': 'https://www.abola.pt/rss/motores',
     'bbc.com/football': 'https://feeds.bbci.co.uk/sport/football/rss.xml',
     'sportstar.thehindu.com/football': 'https://sportstar.thehindu.com/football/feeder/default.rss',
     'sportstar.thehindu.com/football-highlights': 'https://sportstar.thehindu.com/football-highlights/feeder/default.rss',
